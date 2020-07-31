78b770e9cbdbcaa93de2f6ef645650bb8b94908d
RSK Tech Labs <68726937+RSK-TECH-LABS@users.noreply.github.com>
2020-08-01 02:26:32 +0530

    Add files via upload

--- js/float-whatsapp.js
+++ js/float-whatsapp.js
@@ -1,12 +1,11 @@
-$(function() {
+$(function myFunction() {
     $('#WAButton').floatingWhatsApp({
       phone: '1231231231', //WhatsApp Business phone number International format-
       headerTitle: 'Chat with us on WhatsApp!', //Popup Title
       popupMessage: 'Hello, how can we help you?', //Popup Message
       showPopup: true, //Enables popup display
+      showOnIE: false,
       buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
-      //headerColor: 'crimson', //Custom header color
-      //backgroundColor: 'crimson', //Custom background button color
       position: "right"    
     });
   });
