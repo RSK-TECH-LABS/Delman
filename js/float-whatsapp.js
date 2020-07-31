35e65696ca779b654ee07f619f4790dbb2d8907e
RSK Tech Labs <68726937+RSK-TECH-LABS@users.noreply.github.com>
2020-08-01 00:40:14 +0530

    Add files via upload

--- js/float-whatsapp.js
+++ js/float-whatsapp.js
@@ -7,7 +7,7 @@ $(function() {
       buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
       //headerColor: 'crimson', //Custom header color
       //backgroundColor: 'crimson', //Custom background button color
-      position: "left"    
+      position: "right"    
     });
   });
   
